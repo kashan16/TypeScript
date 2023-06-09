4a4eb3a3f2e3fd92b506111a85bb565956019aa5
Mohd Kashan Yunus <106725612+kashan16@users.noreply.github.com>
2023-06-09 19:59:35 +0530

    Delete helloworld.ts

--- helloworld.ts
+++
@@ -1,6 +0,0 @@
-let message : string = "Hello world";
-// create a new heading 1 element 
-let heading = document.createElement('h1');
-heading.textContent = message;
-// add the heading the document
-document.body.appendChild(heading);
